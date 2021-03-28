3cf48c4d0a0325c2fb598654c7422a893201e161
TriNguyen96 <ngoctri2207@gmail.com>
2021-03-28 19:54:15 +0700

    edit card menubot

--- src/components/MenuBot/index.js
+++ src/components/MenuBot/index.js
@@ -5,21 +5,33 @@ import "slick-carousel/slick/slick-theme.css";
 import CardHeader from '../Card/CardHeader';
 
 const dataCardMenu= [
-    title="abc",
+    {title :"1"},
+    {title:"2"},
+    {title:"3"},
+    {title:"4"},
+    {title :"5"},
+    {title:"6"},
+    {title:"7"},
+    {title:"8"},
 ]
 const MenuBot = () => {
     var settings = {
-        dots: true,
+        dots: false,
         dotsClass: "slick-dots slick-thumb",
         infinite: true,
-        speed: 500,
-        slidesToShow: 1,
+        speed: 1000,
+        arrows: false,
+        autoplay: true,
+        slidesToShow: 4,
         slidesToScroll: 1
       };
     return(
         <div className="MenuBot">
            <Slider {...settings} className="banner_slider">
-                <CardHeader title="dfdfd"/>
+               {dataCardMenu.map((item,index) => (
+                        <CardHeader key={index} title={item.title}/>
+               ))}
+                
             </Slider>
         </div>
     )
